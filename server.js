5c17cba7e5af21ca8214ae037b7a208bca1d3cfa
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-01-02 10:25:15 +0530

    some updates

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: process.env.FRONTEND_URL || 'http://16.170.204.86:5000',
     credentials: true
 }));
 app.use(express.json());
