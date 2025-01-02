6f83ed1435b0c8d5968843f2bf5054e108e7dd55
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-01-02 10:38:13 +0530

    some

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.get('/', (req, res) => {
     res.json({ message: 'Welcome to Candidate Management System API' });
 });
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
