aa4250e73a3efd51607cf7b2f564cb90ad8c7f9f
RASHID C <117998515+Rashid-C@users.noreply.github.com>
2025-01-02 10:37:03 +0530

    test

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(process.env.PORT, '0.0.0.0', () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-  });
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
